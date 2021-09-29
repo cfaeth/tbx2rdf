8214526dee7bdf98a4ddda497ac91828c44e01ee
cfaeth <Faeth@em.uni-frankfurt.de>
2021-09-29 20:45:04 +0200

    fixed package in run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ HOME=$( dirname -- "$(realpath -- "$0")");
 
 cd $HOME
 
-mvn exec:java --quiet -e -Dfile.encoding=UTF8 -Dexec.mainClass=org.acoli.fintan.FintanManager -Dexec.args="$*"
+mvn exec:java --quiet -e -Dfile.encoding=UTF8 -Dexec.mainClass=org.acoli.fintan.core.FintanManager -Dexec.args="$*"
 
