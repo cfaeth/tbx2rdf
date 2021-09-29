9e737747c96c15f07bdf03781651ddd0030dc1ea
cfaeth <Faeth@em.uni-frankfurt.de>
2021-09-29 20:40:51 +0200

    this branch contains changes to directly run tbx2rdf integrated with Fintan.
    pom.xml has fintan-core as dependency
    
    build.sh fetches fintan-core (since it is not yet on a Maven repo) and builds all sources.
    run.sh runs tbx2rdf with org.acoli.fintan.core.FintanManager as its main class.
    
    org.acoli.fintan.load.TBX2RDFStreamTransformer.java implements a Fintan loader component
    
    samples/fintan/ contains demo pipelines and an exemplary bash script to howToRun a pipeline.

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#in case mvn does not find java commands like java, javac, javadoc:
+#uncomment the line below and adjust the path to your system
+#JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
+
+# store the path of the conll-rdf directory in HOME
+HOME=$( dirname -- "$(realpath -- "$0")");
+
+cd $HOME
+git clone https://github.com/acoli-repo/fintan-core.git
+cd fintan-core
+mvn --batch-mode --quiet -DskipTests clean install
+
+cd $HOME
+mvn --batch-mode clean install
---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# store the path of the root directory in HOME
+HOME=$( dirname -- "$(realpath -- "$0")");
+
+cd $HOME
+
+mvn exec:java --quiet -e -Dfile.encoding=UTF8 -Dexec.mainClass=org.acoli.fintan.FintanManager -Dexec.args="$*"
+
